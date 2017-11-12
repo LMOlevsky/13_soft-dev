9ef7ebb968a98c497ea6ae87930f5e8140af5f17
Levi <lolevsky@stuy.edu>
2017-11-12 14:35:38 -0500

    added img from tba api

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ import urllib2, json
 
 app = Flask(__name__)
 
-u = urllib2.urlopen("https://api.nasa.gov/planetary/earth/imagery?lon=-74.013871&lat=40.717984&date=2017-02-01&cloud_score=False&api_key=o8PHFoXwiKZ4W4gafFk2VV1JaxxyMGzuhXSjToQ8")
+req = urllib2.Request("https://www.thebluealliance.com/api/v3/team/frc694/media/2017?X-TBA-Auth-Key=jlTx0fXi4VSUc8IEtgq4mNixSvBxx0H6Tmzmlk9ScTDsYPLG7N6F5MSd3gqXQJ00", headers={ 'User-Agent': 'Mozilla/5.0' })
+json_str = urllib2.urlopen(req).read()
+d = json.loads(json_str)
 
-U = u.read()
 
-rest = json.loads(U)
+req2 = urllib2.Request("https://api.nasa.gov/planetary/apod?api_key=o8PHFoXwiKZ4W4gafFk2VV1JaxxyMGzuhXSjToQ8", headers={ 'User-Agent': 'Mozilla/5.0' })
+json_str2 = urllib2.urlopen(req2).read()
+d2 = json.loads(json_str2)
 
 @app.route("/", methods=['POST', 'GET'])
 def root():
-    return render_template("temp.html", things=rest["url"])
+    return render_template("temp.html", tba=d[3]["details"]["thumbnail_url"], nasa=d2["url"])
 
 
 if __name__ == '__main__':    
